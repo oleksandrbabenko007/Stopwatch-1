06c5419cad3b448fe2b3a9315d13aec4a414d9df
natahin <nata_hin@mail.ru>
2016-04-26 13:02:35 +0300

    Added files via upload

---
+++ userScript.js
@@ -0,0 +1,91 @@
+"use strict";
+var stopwatch = $(".wrapper").find(".stopwatch"); 
+var lapsText = $(".wrapper").find(".laps");
+var btnStartStop = $(".wrapper").find(".start_stop_btn");
+var btnClear = $(".wrapper").find(".clear_btn");
+var btnLaps = $(".wrapper").find(".laps_btn");
+var timeInterval = 0;
+var countLaps = 0;
+var firstStartTime = 0;
+var pauseTime = 0;
+
+btnClear.on("click",function(){
+    clearStopwatch();
+});
+
+btnStartStop.on("click",function(){
+    if(stopwatch.hasClass("notactive")){
+        startStopwatch();
+    }
+    else {
+        stopStopwatch();
+    }
+});
+
+btnLaps.on("click", function(){
+    createLap();
+});
+
+function startStopwatch() {
+
+    clearInterval(timeInterval);
+
+    firstStartTime = new Date().getTime();
+
+    timeInterval = setInterval(function() {
+        var newTime = (new Date().getTime() - firstStartTime + pauseTime);
+        stopwatch.text(formatTimeToString(newTime));
+    }, 100);
+
+    btnStartStop.attr("value", "Stop");
+    stopwatch.removeClass("notactive");
+}
+
+function stopStopwatch(){
+
+    clearInterval(timeInterval);
+
+    if(firstStartTime > 0){
+
+        pauseTime = pauseTime + new Date().getTime() - firstStartTime;       
+        firstStartTime = 0;
+    }
+    
+    btnStartStop.attr("value","Start");
+    stopwatch.addClass("notactive");
+}
+
+function createLap(){
+    if(!stopwatch.hasClass("notactive")) {
+        lapsText.append("Lap #" + (++countLaps) + ":  " + stopwatch.text() + '<br/>');
+    }
+}
+
+function clearStopwatch(){
+
+    clearInterval(timeInterval);
+    btnStartStop.text("Start");
+    stopwatch.addClass('notactive');
+    stopwatch.text(formatTimeToString(0));
+    lapsText.text("");
+
+    firstStartTime = 0;
+    pauseTime = 0;
+    countLaps = 0;
+}
+
+function formatTimeToString(time){
+    var hours, minutes, seconds, milliseconds;
+    
+    milliseconds = Math.floor((time % 1000)/100);
+    seconds = Math.floor((time/1000) % 60);
+    minutes = Math.floor((time/(1000*60)) % 60);
+    hours = Math.floor(time/(1000*60*60) % 24);
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    hours = hours < 10 ? '0' + hours : hours;
+
+    return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
+
